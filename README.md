936a61244d5016c88badd081859d0c6dc8c7c297
Jack Serrino <Jserrino@gmail.com>
2018-03-20 10:40:09 +0800

    Add download script

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # faster-rcnn-demo
 A demo of faster-rcnn running in tensorflow on iOS and Android
+
+## Setup
+
+First, download the needed model.
+
+```
+scripts/download_model.sh
+```
+
+### iOS
+
+Requirements: xcode, cocoapods
+
+```
+cd ios/simple
+pod install
+cd ../camera
+pod install
+```
+
+### Android
+
+Requirements: ???
+
+```
+??? tbd
+```
---
+++ scripts/download_models.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+cd $PROJECT_DIR
+
+MODEL_BASE=http://download.tensorflow.org/models/object_detection
+
+for MODEL_ZIP in faster_rcnn_inception_v2_coco_2018_01_28.tar.gz faster_rcnn_resnet50_lowproposals_coco_2018_01_28.tar.gz
+do
+  curl -o ./assets/${MODEL_ZIP} ${MODEL_BASE}/${MODEL_ZIP}
+  tar xvf ./assets/${MODEL_ZIP} -C ./assets
+done
+ 
